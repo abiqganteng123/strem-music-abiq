a9c062a8d4d7c10f9d5e405643ede8b3e89739db
abiq <164733837+abiqganteng123@users.noreply.github.com>
2025-03-25 22:03:13 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+// Service Worker for background audio playback
+self.addEventListener('install', event => {
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
